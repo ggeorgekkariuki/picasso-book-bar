8770e5ad94ed42f8cdab903d5d57cb861283d08a
George Kariuki <gconsummer.gk@gmail.com>
2024-07-11 14:11:52 +0300

    Add BookDetails class

--- functions.py
+++ functions.py
@@ -1,4 +1,34 @@
 from pickles import *
+from pickles import *
+import numpy as np
+
+class BookDetails():
+    def __init__(self, book_idx):
+        self.book_idx = book_idx
+        self.df_idx = np.where(final_rating['title'] == book_pivot.index[self.book_idx])[0][0]
+
+    def fetch_book(self):
+        # Find the location of the book in the main df
+        # Book
+        return book_pivot.index[self.book_idx]
+
+    def fetch_poster(self):
+        # URLs
+        return final_rating.iloc[self.df_idx]['img_url']
+
+    def fetch_author(self):
+        # URLs
+        return final_rating.iloc[self.df_idx]['author']
+
+
+def fetch_poster(suggestion):
+    """Obtains the poster for the book"""
+    poster_urls = []
+    for idx in suggestion[0]:
+        book = BookDetails(book_idx=idx)
+        return poster_urls.append(book.fetch_poster())
+
+
 
 # Function to recommend books and returns the poster
 def recommend_books(book_name):
