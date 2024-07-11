f62b14b8b3d3fe0472bce6099a1347dac8761f09
George Kariuki <gconsummer.gk@gmail.com>
2024-07-11 17:24:05 +0300

    Add images to the Author Page

--- app.py
+++ app.py
@@ -48,27 +48,32 @@ def authors():
 
     # Create a button that returns a recommendation
     if st.button('Show Author Recommendation'):
-        recommended_books, works = recommended_author(selected_author)
+        recommended_author, book_title, poster = recommended_author(selected_author)
 
         # Define columns for the posters
         col1, col2, col3, col4, col5 = st.columns(5)
 
         # Render the columns
         with col1:
-            st.text(recommended_books[1])# Book Title
-            st.text(works[1]) # Poster
+            st.text(recommended_author[1])# Book Title
+            st.text(book_title[1]) # Poster
+            st.image(poster[1]) # Poster
         with col2:
-            st.text(recommended_books[2])# Book Title
-            st.text(works[2])
+            st.text(recommended_author[2])# Book Title
+            st.text(book_title[2])
+            st.image(poster[2])
         with col3:
-            st.text(recommended_books[3])# Book Title
-            st.text(works[3])
+            st.text(recommended_author[3])# Book Title
+            st.text(book_title[3])
+            st.image(poster[3])
         with col4:
-            st.text(recommended_books[4])# Book Title
-            st.text(works[4])
+            st.text(recommended_author[4])# Book Title
+            st.text(book_title[4])
+            st.image(poster[4])
         with col5:
-            st.text(recommended_books[5])# Book Title
-            st.text(works[5])
+            st.text(recommended_author[5])# Book Title
+            st.text(book_title[5]) # Poster
+            st.image(poster[5])
 
 page_names_to_funcs = {
     # "—": intro,
