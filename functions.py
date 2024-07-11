7a3f2ea88c38e7a21021576adf5ac60e671b7245
George Kariuki <gconsummer.gk@gmail.com>
2024-07-11 14:11:17 +0300

    Reformatting the recommend_books func

--- functions.py
+++ functions.py
@@ -1,15 +1,23 @@
-# Get the poster URL
-def get_book_details(book_title):
-    """Get the ISBN, author and Image URL given an exact title"""
-    return final_rating[final_rating['title'] == book_title].sort_values(by='year', ascending=False)\
-            .iloc[0,:][['ISBN', 'author', 'img_url']]
+from pickles import *
 
 # Function to recommend books and returns the poster
 def recommend_books(book_name):
-    """This function takes a book name and returns other book suggestions"""
+    """This function takes a booklist of name and returns other book suggestions"""
     # Obtain the book ID
     book_id = np.where(book_pivot.index == book_name)
+
     # Obtain the whole record
     record = book_pivot.iloc[book_id, :].values.reshape(1, -1)
+
     # Distances and suggestions
     distance, suggestion = book_model.kneighbors(record, n_neighbors=6)
+
+    # Empty book list to store the books
+    book_list = []
+
+    for i in range(len(suggestion)):
+        book_names = book_pivot.index[suggestion[i]]
+        for j in book_names:
+            book_list.append(j)
+
+    return book_list, poster
