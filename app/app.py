b1c9ec68794056aab69ba3c6a66f65f1f6ea26c4
George Kariuki <gconsummer.gk@gmail.com>
2024-07-14 11:29:44 +0300

    Add ratings section

--- app/app.py
+++ app/app.py
@@ -20,7 +20,7 @@ def books():
     import streamlit as st
     import numpy as np
     from pickles import book_names
-    from functions import recommend_books, recommended_author
+    from functions import recommend_books
 
     # Webpage Titles
     st.header("Picasso's Bar - But For Books")
@@ -31,7 +31,7 @@ def books():
 
     # Create a button that returns a recommendation
     if st.button('Show Book Recommendation'):
-        recommended_books, posters = recommend_books(selected_books)
+        recommended_books, posters, authors, ratings = recommend_books(selected_books)
 
         # Define columns for the posters
         col1, col2, col3, col4, col5 = st.columns(5)
@@ -39,19 +39,29 @@ def books():
         # Render the columns
         with col1:
             st.text(recommended_books[1])# Book Title
+            st.text(authors[1])# Book Title
             st.image(posters[1]) # Poster
+            st.write(f"Rated: {ratings[1]}") # Ratings
         with col2:
-            st.text(recommended_books[2])# Book Title
+            st.text(recommended_books[2])
+            st.text(authors[2])
             st.image(posters[2])
+            st.write(f"Rated: {ratings[2]}") # Ratings
         with col3:
-            st.text(recommended_books[3])# Book Title
+            st.text(recommended_books[3])
+            st.text(authors[3])
             st.image(posters[3])
+            st.write(f"Rated: {ratings[3]}") # Ratings
         with col4:
-            st.text(recommended_books[4])# Book Title
+            st.text(recommended_books[4])
+            st.text(authors[4])
             st.image(posters[4])
+            st.write(f"Rated: {ratings[4]}") # Ratings
         with col5:
-            st.text(recommended_books[5])# Book Title
+            st.text(recommended_books[5])
+            st.text(authors[5])
             st.image(posters[5])
+            st.write(f"Rated: {ratings[5]}") # Ratings
 
 def authors():
 
@@ -68,7 +78,7 @@ def authors():
 
     # Create a button that returns a recommendation
     if st.button('Show Author Recommendation'):
-        recommended_author, book_title, poster = recommended_author(selected_author)
+        recommended_author, book_title, poster, ratings = recommended_author(selected_author)
 
         # Define columns for the posters
         col1, col2, col3, col4, col5 = st.columns(5)
@@ -77,18 +87,23 @@ def authors():
         with col1:
             st.text(book_title[1]) # Title
             st.image(poster[1]) # Poster
+            st.write(f"Rated: {ratings[1]}")
         with col2:
             st.text(book_title[2])
             st.image(poster[2])
+            st.write(f"Rated: {ratings[2]}")
         with col3:
             st.text(book_title[3])
             st.image(poster[3])
+            st.write(f"Rated: {ratings[3]}")
         with col4:
             st.text(book_title[4])
             st.image(poster[4])
+            st.write(f"Rated: {ratings[4]}")
         with col5:
             st.text(book_title[5]) 
             st.image(poster[5])
+            st.write(f"Rated: {ratings[5]}")
 
 # Pages
 page_names_to_funcs = {
